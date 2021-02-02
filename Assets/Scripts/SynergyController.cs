120bc8d08909d986fa6afc94bc902efe9d94eeb4
Radoslav Chobanov <rado.chobanov97@gmail.com>
2021-02-02 23:35:04 +0200

    Update SynergyController.cs
    Event handler

--- Assets/Scripts/SynergyController.cs
+++ Assets/Scripts/SynergyController.cs
@@ -1,33 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class SynergyController
+public class SynergyController : MonoBehaviour
 {
+    //singleton -----------------------------------------------------------
+    public static SynergyController singleton;
+    //---------------------------------------------------------------------
+
     // race counters
     private int humanCounter;
     private int orcCounter;
     private int elfCounter;
-
+        
     // class counters
     private int warriorCounter;
     private int archerCounter;
     private int mageCounter;
-
-    // buffs for synergies vars
-
-        //human vars
-    bool haveHumanBuff3 = false;
-    bool haveHumanBuff6 = false;
-
-        //orc vars
-    bool haveOrcBuff3 = false;
-    bool haveOrcBuff6 = false;
-
-        //elf vars
-    bool haveElfBuff3 = false;
-    bool haveElfBuff6 = false;
 
     // constructors
     public SynergyController()
@@ -43,11 +35,17 @@ public class SynergyController
         MageCounter = 0;
     }
 
+
     // getters and setters
     public int HumanCounter
     {
         get { return humanCounter; }
-        set { humanCounter = value; }
+        set 
+        { 
+            humanCounter = value;
+            if (humanCounter == 3) ; // invoke event to give human buff 3, and 6, etc.
+
+        }
     }
     public int OrcCounter
     {
@@ -75,6 +73,19 @@ public class SynergyController
         set { mageCounter = value; }
     }
 
+    private void Awake()
+    {
+        if (singleton != null && singleton != this)
+            Destroy(this.gameObject);
+        else
+            singleton = this;
+    }
+
+    private void Start()
+    {
+        TacticsMove.singleton.ChampionEnteredBattlefield.AddListener(() => UpdateSynergies());
+    }
+
     public void ResetCounters()
     {        
         //races
@@ -97,6 +108,20 @@ public class SynergyController
         MonoBehaviour.print("Archers - " + ArcherCounter);
         MonoBehaviour.print("Mages - " + MageCounter);
     }
+    public void UpdateSynergies()
+    {
+        print("Update synergies");
+
+        ResetCounters();
+
+        foreach (GameObject champObj in TacticsMove.singleton.champions)
+        {
+            Champion champ = champObj.GetComponent<Champion>();
+            AddIntoSyngeries(champ);
+        }
+
+        TacticsMove.singleton.SynergiesUpdated.Invoke();
+    }
     public void AddIntoSyngeries(Champion champ)
     {
         Race tempRace = champ._race;
@@ -117,8 +142,6 @@ public class SynergyController
         if (tempClass == Class.Mage)
             MageCounter++;
         // ...
-
-        UpdateBonuses();
     }
     public void RemoveFromSynergies(Champion champ)
     {
@@ -138,162 +161,5 @@ public class SynergyController
             ArcherCounter--;
         if (tempClass == Class.Mage)
             MageCounter--;
-
-        UpdateBonuses();
     }
-
-    public void UpdateBonuses() // updating all the bonuses
-    {
-        //human bonuses
-        if (humanCounter < 3 && haveHumanBuff3 == true)
-        {
-            RemoveHumanBuff3();
-            haveHumanBuff3 = false;
-        }
-        else if (humanCounter >= 3 && humanCounter <= 5 && haveHumanBuff3 == false && haveHumanBuff6 == false)
-        {
-            GiveHumanBuff3();
-            haveHumanBuff3 = true;
-        }
-        else if (humanCounter >= 3 && humanCounter <= 5 && haveHumanBuff3 == false && haveHumanBuff6 == true)
-        {
-            RemoveHumanBuff6();
-            haveHumanBuff6 = false;
-            GiveHumanBuff3();
-            haveHumanBuff3 = true;
-        }
-        else if (humanCounter >= 6 && haveHumanBuff3 == true && haveHumanBuff6 == false)
-        {
-            RemoveHumanBuff3();
-            haveHumanBuff3 = false;
-            GiveHumanBuff6();
-            haveHumanBuff6 = true;
-        }
-
-        //orc bonuses
-        if (orcCounter < 3)
-        {
-            RemoveOrcBuff3();
-            haveOrcBuff3 = false;
-        }
-        else if (orcCounter >= 3 && orcCounter <= 5 && haveOrcBuff3 == false && haveOrcBuff6 == false)
-        {
-            GiveOrcBuff3();
-            haveOrcBuff3 = true;
-        }
-        else if (orcCounter >= 3 && orcCounter <= 5 && haveOrcBuff3 == false && haveOrcBuff6 == true)
-        {
-            RemoveOrcBuff6();
-            haveOrcBuff6 = false;
-            GiveOrcBuff3();
-            haveOrcBuff3 = true;
-        }
-        else if (orcCounter >= 6 && haveOrcBuff3 == true && haveOrcBuff6 == false)
-        {
-            RemoveOrcBuff3();
-            haveOrcBuff3 = false;
-            GiveOrcBuff6();
-            haveOrcBuff6 = true;
-        }
-
-        //elf bonuses
-    }
-
-    public void GiveHumanBuff3()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Human && champ.GetComponent<ChampionController>().GetCurrentTile().isHomeBattlefieldTile)
-            {
-                champ.buffs.Add(Buff.Human3);
-            }
-        }
-    }
-    public void RemoveHumanBuff3()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Human)
-            {
-                champ.buffs.Remove(Buff.Human3);
-            }
-        }
-    }
-    public void GiveHumanBuff6()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Human)
-            {
-                champ.buffs.Add(Buff.Human6);
-            }
-        }
-    }
-    public void RemoveHumanBuff6()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Human)
-            {
-                champ.buffs.Remove(Buff.Human6);
-            }
-        }
-    }
-
-    public void GiveOrcBuff3()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Orc)
-            {
-                champ.buffs.Add(Buff.Orc3);
-            }
-        }
-    }
-    public void RemoveOrcBuff3()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Orc)
-            {
-                champ.buffs.Remove(Buff.Orc3);
-            }
-        }
-    }
-    public void GiveOrcBuff6()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Orc)
-            {
-                champ.buffs.Add(Buff.Orc6);
-            }
-        }
-    }
-    public void RemoveOrcBuff6()
-    {
-        foreach (GameObject champObj in TacticsMove.singleton.champions)
-        {
-            Champion champ = champObj.GetComponent<Champion>();
-
-            if (champ._race == Race.Orc)
-            {
-                champ.buffs.Remove(Buff.Orc6);
-            }
-        }
-    }
-}
+}   
