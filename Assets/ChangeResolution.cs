9a24e2e7617f52f68d44cfec4a9c563b279f5d85
Radoslav Chobanov <rado.chobanov97@gmail.com>
2021-05-08 19:44:18 +0300

    Settings Scene added, Resolution dropdown menu added

---
+++ Assets/ChangeResolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChangeResolution : MonoBehaviour
+{
+    private DropdownMenu resolutionDropdown;
+
+    void Start()
+    {
+        resolutionDropdown = this.gameObject.GetComponent<DropdownMenu>();
+
+        print(resolutionDropdown.MenuItems());
+    }
+}
---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,15 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    public GameObject resolutionDropdownObj;
+    private Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutionDropdown = resolutionDropdownObj.GetComponent<Dropdown>();
+    }
+}
